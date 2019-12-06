073c24041ed67af4b383fbd4e4c6067bcfb97ae4
Ryan LaCombe <ryanalacombe@gmail.com>
2019-12-06 12:30:27 -0600

    Add project files.

---
+++ MathClasses/Colour.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClasses
+{
+    public class Colour
+    {
+        public UInt32 colour;
+
+        public Colour(int red, int green, int blue, int alpha)
+        {
+            colour = 0;
+        }
+
+        public int GetRed()
+        {
+            return 0;
+        }
+        public int SetRed(int red)
+        {
+            return 0;
+        }
+
+        public int GetGreen()
+        {
+
+        }
+        public void SetGreen()
+        {
+
+        }
+
+        public void GetBlue()
+        {
+
+        }
+        public void SetBlue()
+        {
+
+        }
+
+        public void GetAlpha()
+        {
+
+        }
+        public void SetAlpha()
+        {
+
+        }
+    }
+}
